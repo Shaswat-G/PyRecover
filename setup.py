44c5b5385a2f37753e092884d9a2de9e71660c6b
Shaswat Gupta <shagupta@ethz.ch>
2025-05-05 09:29:35 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyrecover",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "torch>=1.13.0",
+        "tqdm",
+        "pyyaml",
+    ],
+    author="Shaswat Gupta",
+    author_email="shaswat.gupta.iitb@gmail.com",
+    description="Distributed checkpointing manager for SLURM environments",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/YOUR_USERNAME/pyrecover",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires=">=3.8",
+)
